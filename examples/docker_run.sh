21ee4d34bc77ffcf580b181fc7082d459e722558
Brian Magnuson <bmagnuso@umich.edu>
2026-06-30 15:33:59 -0400

    fix docker and cloud config

--- examples/docker_run.sh
+++ examples/docker_run.sh
@@ -7,5 +7,15 @@ base_dir=$(pwd)
 docker_image="bmagnuso/crispr:0.0.21"
 input_dir="input"
 output_dir="output"
-docker run --rm -itd --name $container_name -v $base_dir:/repo -v $base_dir/$output_dir:/output -v $base_dir/$input_dir:/input -w /repo $docker_image bash
+
+docker run --rm -itd \
+    --name $container_name \
+    --user $(id -u):$(id -g) \
+    -v $base_dir:/repo \
+    -v $base_dir/$output_dir:/output \
+    -v $base_dir/$input_dir:/input \
+    -v ~/.config/gcloud:/tmp/host-gcloud:ro \
+    -w /repo \
+    $docker_image bash
+
 docker attach $container_name
